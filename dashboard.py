6f595990c6b2df161989abcdc73c74267024183a
Himagna Ganti <himagna.ganti6@gmail.com>
2023-11-11 14:17:37 -0500

    First commit in working branch

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ pio.renderers.default = 'browser'
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
 
 st.title(" 1st Vis")
-st.header('What ius this')
+st.header('What ius')
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
 
@@ -31,27 +31,3 @@ st.plotly_chart(fig)
 
 
 
-st.title(" 2nd Vis")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
-
-#for vis 2
-options2 = st.multiselect('Time-Frame?', ['All-time', '2001-2010','2011-2020' ,'1991-2000'])
-
-
-#for vis3
-st.title(" 3rd Vis")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
-
-
-data = dict(
-    character=["", "MOnza", "Tokyo", "MOntreal", "Miami", 'Sakhir','Singapore'],
-    parent=["", "2013", "2014", "2015", "2016", "2017", "2018" ],
-    value=[10, 14, 12, 10, 2, 6,8])
-
-fig3 = px.sunburst(
-    data,
-    names='character',
-    parents='parent',
-    values='value',
-)
-st.plotly_chart(fig3)
